da8ee7d30cdc5dd16c45e3d9fe3c3b1dec39b8bd
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2023-08-07 10:27:40 +0530

    feat: completed UI

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -2,6 +2,7 @@ import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { dark } from '@clerk/themes'
 
 function Topbar() {
   return (
@@ -27,6 +28,7 @@ function Topbar() {
             <OrganizationSwitcher
             appearance={
                 {
+                    baseTheme: dark ,
                     elements:{
                         organisationSwitcherTrigger : "py-2 px-4"
                     }
